be15049dc3f7b7b737404ccbd01f594fac86b4d1
jeremy <517429+JeremyMelton@users.noreply.github.com>
2024-02-13 19:26:34 -0700

    initial commit

---
+++ FluidTemplateEngine.cs
@@ -0,0 +1,44 @@
+﻿// ﻿﻿Copyright (c) Code Impressions, LLC. All Rights Reserved.
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License")
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+using Fluid;
+using System.Threading.Tasks;
+using Transmitly.Template.Configuration;
+
+namespace Transmitly
+{
+	internal class FluidTemplateEngine : ITemplateEngine
+	{
+		private readonly FluidParser _parser;
+		public FluidTemplateEngine(FluidParserOptions fluidParserOptions)
+		{
+			Guard.AgainstNull(fluidParserOptions);
+			_parser = new FluidParser(fluidParserOptions);
+		}
+
+		public async Task<string?> RenderAsync(IContentTemplateRegistration? registration, IContentModel? contentModel)
+		{
+			if (registration == null || contentModel == null)
+				return null;
+			var source = await registration.GetContentAsync();
+			if (_parser.TryParse(source, out var template, out var error))
+			{
+				var context = new TemplateContext(contentModel.Model);
+				return template.Render(context);
+			}
+			System.Diagnostics.Debug.WriteLine($"{nameof(FluidTemplateEngine)} {error}");
+			return null;
+		}
+	}
+}
---
+++ FluidTemplateEngineExtensions.cs
@@ -0,0 +1,58 @@
+﻿// ﻿﻿Copyright (c) Code Impressions, LLC. All Rights Reserved.
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License")
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+using Fluid;
+using System;
+using Transmitly.Template.Configuration;
+
+namespace Transmitly
+{
+	public static class FluidTemplateEngineExtensions
+	{
+		private const string FluidId = "Fluid";
+		private const string DefaultProviderId = "Default";
+
+		public static string Fluid(this TemplateEngines templateEngines, string? providerId = DefaultProviderId)
+		{
+			Guard.AgainstNull(templateEngines);
+
+			return $"{FluidId}.{(!string.IsNullOrWhiteSpace(providerId) ? providerId : DefaultProviderId)}";
+		}
+
+		public static CommunicationsClientBuilder AddFluidTemplateEngine(this TemplateConfigurationBuilder templateConfiguration, Action<FluidParserOptions> options, string? templateEngineId = null)
+		{
+			Guard.AgainstNull(templateConfiguration);
+			Guard.AgainstNull(options);
+
+			var opts = new FluidParserOptions();
+			options(opts);
+			return templateConfiguration.Add(new FluidTemplateEngine(opts), Id.TemplateEngines.Fluid(templateEngineId));
+		}
+
+		public static CommunicationsClientBuilder AddFluidTemplateEngine(this TemplateConfigurationBuilder templateConfiguration, string? templateEngineId = null)
+		{
+			return AddFluidTemplateEngine(templateConfiguration, (opts) => { }, templateEngineId);
+		}
+
+		public static CommunicationsClientBuilder AddFluidTemplateEngine(this CommunicationsClientBuilder communicationsClientBuilder)
+		{
+			return AddFluidTemplateEngine(communicationsClientBuilder.TemplateEngine, (opts) => { });
+		}
+
+		public static CommunicationsClientBuilder AddFluidTemplating(this CommunicationsClientBuilder communicationsClientBuilder, Action<FluidParserOptions> options)
+		{
+			return AddFluidTemplateEngine(communicationsClientBuilder.TemplateEngine, options, Id.TemplateEngines.Fluid());
+		}
+	}
+}
