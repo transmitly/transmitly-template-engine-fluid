8f7097cdc83368aee3b0de8447c8a0c9b0e930d7
jeremy <517429+JeremyMelton@users.noreply.github.com>
2026-03-24 23:54:30 -0600

    chore: update packages

--- src/Transmitly.TemplateEngine.Fluid/FluidTemplateEngineExtensions.cs
+++ src/Transmitly.TemplateEngine.Fluid/FluidTemplateEngineExtensions.cs
@@ -1,4 +1,4 @@
-﻿// ﻿﻿Copyright (c) Code Impressions, LLC. All Rights Reserved.
+﻿// Copyright (c) Code Impressions, LLC. All Rights Reserved.
 //  
 //  Licensed under the Apache License, Version 2.0 (the "License")
 //  you may not use this file except in compliance with the License.
@@ -19,10 +19,19 @@ using Transmitly.Util;
 
 namespace Transmitly
 {
+	/// <summary>
+	/// Provides extension methods for registering and resolving the Fluid template engine.
+	/// </summary>
 	public static class FluidTemplateEngineExtensions
 	{
 		private const string FluidId = "Fluid";
 
+		/// <summary>
+		/// Gets the template engine identifier for the Fluid template engine.
+		/// </summary>
+		/// <param name="templateEngines">The template engine identifier builder.</param>
+		/// <param name="providerId">An optional provider identifier to scope the template engine identifier.</param>
+		/// <returns>The template engine identifier for the Fluid template engine.</returns>
 		public static string Fluid(this TemplateEngines templateEngines, string? providerId = null)
 		{
 			Guard.AgainstNull(templateEngines);
@@ -30,6 +39,13 @@ namespace Transmitly
 			return templateEngines.GetId(FluidId, providerId);
 		}
 
+		/// <summary>
+		/// Registers the Fluid template engine using the provided parser configuration.
+		/// </summary>
+		/// <param name="templateConfiguration">The template configuration builder to update.</param>
+		/// <param name="options">Configures the <see cref="FluidParserOptions" /> used by the Fluid parser.</param>
+		/// <param name="templateEngineId">An optional template engine identifier override.</param>
+		/// <returns>The communications client builder.</returns>
 		public static CommunicationsClientBuilder AddFluidTemplateEngine(this TemplateConfigurationBuilder templateConfiguration, Action<FluidParserOptions> options, string? templateEngineId = null)
 		{
 			Guard.AgainstNull(templateConfiguration);
@@ -40,16 +56,33 @@ namespace Transmitly
 			return templateConfiguration.Add(new FluidTemplateEngine(opts), Id.TemplateEngines.Fluid(templateEngineId));
 		}
 
+		/// <summary>
+		/// Registers the Fluid template engine with default parser options.
+		/// </summary>
+		/// <param name="templateConfiguration">The template configuration builder to update.</param>
+		/// <param name="templateEngineId">An optional template engine identifier override.</param>
+		/// <returns>The communications client builder.</returns>
 		public static CommunicationsClientBuilder AddFluidTemplateEngine(this TemplateConfigurationBuilder templateConfiguration, string? templateEngineId = null)
 		{
 			return AddFluidTemplateEngine(templateConfiguration, (opts) => { }, templateEngineId);
 		}
 
+		/// <summary>
+		/// Registers the Fluid template engine on the communications client builder with default parser options.
+		/// </summary>
+		/// <param name="communicationsClientBuilder">The communications client builder to update.</param>
+		/// <returns>The communications client builder.</returns>
 		public static CommunicationsClientBuilder AddFluidTemplateEngine(this CommunicationsClientBuilder communicationsClientBuilder)
 		{
 			return AddFluidTemplateEngine(communicationsClientBuilder.TemplateEngine, (opts) => { });
 		}
 
+		/// <summary>
+		/// Registers the Fluid template engine on the communications client builder using the provided parser configuration.
+		/// </summary>
+		/// <param name="communicationsClientBuilder">The communications client builder to update.</param>
+		/// <param name="options">Configures the <see cref="FluidParserOptions" /> used by the Fluid parser.</param>
+		/// <returns>The communications client builder.</returns>
 		public static CommunicationsClientBuilder AddFluidTemplateEngine(this CommunicationsClientBuilder communicationsClientBuilder, Action<FluidParserOptions> options)
 		{
 			return AddFluidTemplateEngine(communicationsClientBuilder.TemplateEngine, options, Id.TemplateEngines.Fluid());
